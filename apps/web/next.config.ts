e4abe1b04a46e49551e7ccd319d18aa014561edf
Bee <bernicethomas@icloud.com>
2026-03-19 14:44:33 +0000

    fix build err
    fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,8 +2,6 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  outputFileTracingRoot: path.join(__dirname, "../../"),
   turbopack: {
     root: path.join(__dirname, "../../"),
   },
