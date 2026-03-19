386537c33b779c4dab7758cd1c5a2e104c8e9a37
Bee <bernicethomas@icloud.com>
2026-03-19 14:30:47 +0000

    update stripe version

--- apps/api/src/stripe/index.ts
+++ apps/api/src/stripe/index.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 import { env } from "../lib/config.js";
 
 export const stripe = env.STRIPE_SECRET_KEY
-  ? new Stripe(env.STRIPE_SECRET_KEY, { apiVersion: "2025-02-24.acacia" })
+  ? new Stripe(env.STRIPE_SECRET_KEY, { apiVersion: "2026-02-25.clover" })
   : null;
